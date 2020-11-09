7e2180dba5d984227d6e9f64302582a27a5b8024
syeda-sumreen-ali <sumreenali111@gmail.com>
2020-11-09 10:39:03 +0500

    initial server and passing data from static json file from server

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const products = require('./data/products')
+
+const app = express()
+const port = 5000
+
+app.get('/', (req, res) => {
+	res.send("API is running...")
+})
+
+app.get('/api/products', (req, res) => {
+	res.json(products)
+})
+
+app.get('/api/product/:id', (req, res) => {
+	const product = products.find(p => p._id === req.params.id)
+	res.json(product)
+})
+
+app.listen(port, console.log(`Server is running on port ${port}`))
